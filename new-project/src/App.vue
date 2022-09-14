dfa68b6a8dfe6b87ef0d4c77cfb9dded531c6634
vladislav <vl_evlashin@mail.ru>
2022-09-14 18:31:04 +0300

    Сделал первое дз

--- new-project/src/App.vue
+++ new-project/src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" class="operand1" v-model="operand1">
+    <span class="operator"> {{ operator }} </span>
+    <input type="text" class="operand2" v-model="operand2">
+    <span> = </span>
+    <span class="res">{{ res }}</span>
+    <br>
+    <button class="operator-button" v-on:click="operator = '+'">+</button>
+    <button class="operator-button" v-on:click="operator = '-'">-</button>
+    <button class="operator-button" v-on:click="operator = '*'">*</button>
+    <button class="operator-button" v-on:click="operator = '/'">/</button>
+    <button class="operator-button" v-on:click="operator = '**'">**</button>
+    <button class="operator-button" v-on:click="operator = '%'">%</button>
+    <button class="operator-button" v-on:click="calculate">=</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      operand1: '',
+      operand2: '',
+      operator: '+',
+      res: ''
+    }
+  },
+  methods: {
+    calculate() {
+      switch (this.operator) {
+        case '+':
+          this.res = Number(this.operand1) + Number(this.operand2);
+          break;
+        case '-':
+          this.res = Number(this.operand1) - Number(this.operand2);
+          break;
+        case '*':
+          this.res = Number(this.operand1) * Number(this.operand2);
+          break;
+        case '/':
+          this.res = Number(this.operand1) / Number(this.operand2);
+          break;
+        case '**':
+          this.res = Number(this.operand1) ** Number(this.operand2);
+          break;
+        case '%':
+          this.res = Math.floor(Number(this.operand1) / Number(this.operand2));
+          break;
+        default:
+          this.res = Number(this.operand1) + Number(this.operand2);
+          break;
+      }
+    }
   }
 }
 </script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+#app {}
 </style>
